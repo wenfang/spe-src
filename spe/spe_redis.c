e5c3cf1c52f6616f4b3350eec6f499345f83e3ff
wenfang <wangwenfang@gmail.com>
2014-03-21 16:20:45 +0800

    update spe_redis

--- spe/spe_redis.c
+++ spe/spe_redis.c
@@ -1,5 +1,13 @@
 #include "spe_redis.h"
+#include "spe_sock.h"
 #include "spe_log.h"
+#include "spe_util.h"
+
+void
+spe_redis_connect(spe_redis_t* sr, spe_handler_t handler) {
+  ASSERT(sr);
+  spe_conn_connect(sr->conn, sr->host, sr->port, handler);
+}
 
 spe_redis_t*
 spe_redis_create(const char* host, const char* port) {
@@ -8,5 +16,20 @@ spe_redis_create(const char* host, const char* port) {
     SPE_LOG_ERR("spe_redis calloc error");
     return NULL;
   }
+  int cfd = spe_sock_tcp_socket();
+  if (cfd < 0) {
+    SPE_LOG_ERR("spe_sock_tcp_socket error");
+    free(sr);
+    return NULL;
+  }
+  sr->conn = spe_conn_create(cfd);
+  if (!sr->conn) {
+    SPE_LOG_ERR("spe_conn_create error");
+    spe_sock_close(cfd);
+    free(sr);
+    return NULL;
+  }
+  sr->host = host;
+  sr->port = port;
   return sr;
 }
--- spe/spe_redis.h
+++ spe/spe_redis.h
@@ -2,6 +2,7 @@
 #define __SPE_REDIS_H
 
 #include "spe_conn.h"
+#include "spe_handler.h"
 
 struct spe_redis_s {
   spe_conn_t* conn;
@@ -9,6 +10,9 @@ struct spe_redis_s {
   const char* port;
 };
 typedef struct spe_redis_s spe_redis_t;
+
+extern void
+spe_redis_connect(spe_redis_t* sr, spe_handler_t handler);
 
 extern spe_redis_t*
 spe_redis_create(const char* host, const char* port);
