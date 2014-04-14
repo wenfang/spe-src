990b4528708f58baf6ccd2c2a8a1acafbdd3a7b2
wenfang <wangwenfang@gmail.com>
2014-04-14 16:41:14 +0800

    drive machine

--- example/rdspool.c
+++ example/rdspool.c
@@ -4,6 +4,18 @@
 static spe_server_t* srv;
 static spe_pool_t* pool;
 
+#define RDS_INIT  0
+#define RDS_GET   1
+#define RDS_CLOSE 2
+#define RDS_CONN  3
+
+struct spe_rds_s {
+  spe_conn_t*   conn;
+  spe_redis_t*  red;
+  unsigned      status;
+};
+typedef struct spe_rds_s spe_rds_t;
+
 static void
 on_close(void* arg1, void* arg2) {
   spe_conn_t* conn = arg1;
@@ -69,6 +81,78 @@ on_read(void* arg) {
 }
 
 static void
+drive_machine(void* arg) {
+  spe_rds_t* rds = arg;
+  if (rds->red && (rds->red->closed || rds->red->error) && rds->status != RDS_CLOSE) {
+    rds->status = RDS_CLOSE;
+    spe_conn_writes(rds->conn, "ERROR GET DATA\r\n");
+    spe_conn_flush(rds->conn, SPE_HANDLER1(drive_machine, rds));
+    spe_redis_destroy(rds->red);
+    rds->red = NULL;
+    return;
+  }
+  switch (rds->status) {
+    case RDS_INIT:
+      if (rds->conn->error || rds->conn->closed) {
+        spe_conn_destroy(rds->conn);
+        free(rds);
+        return;
+      }
+      rds->red = spe_pool_get(pool);
+      if (rds->red) {
+        rds->status = RDS_GET;
+        spe_redis_do(rds->red, SPE_HANDLER1(drive_machine, rds), 2, "get", "mydokey");
+        return;
+      }
+      rds->red = spe_redis_create("127.0.0.1", "6379");
+      if (!rds->red) {
+        rds->status = RDS_CLOSE;
+        spe_conn_writes(rds->conn, "ERROR CREATE REDIS CLIENT\r\n");
+        spe_conn_flush(rds->conn, SPE_HANDLER1(drive_machine, rds));
+        return;
+      }
+      rds->status = RDS_CONN;
+      spe_redis_connect(rds->red, SPE_HANDLER1(drive_machine, rds));
+      break;
+    case RDS_GET:
+      for (int i=0; i<rds->red->recv_buffer->len; i++) {
+        spe_conn_write(rds->conn, rds->red->recv_buffer->data[i]);
+      }
+      rds->status = RDS_CLOSE;
+      spe_conn_flush(rds->conn, SPE_HANDLER1(drive_machine, rds));
+      break;
+    case RDS_CLOSE:
+      if (rds->red) spe_pool_put(pool, rds->red);
+      spe_conn_destroy(rds->conn);
+      free(rds);
+      break;
+    case RDS_CONN:
+      rds->status = RDS_GET;
+      spe_redis_do(rds->red, SPE_HANDLER1(drive_machine, rds), 2, "get", "mydokey");
+      break;
+  }
+}
+
+static void
+run1(spe_server_t* srv, unsigned cfd) {
+  spe_conn_t* conn = spe_conn_create(cfd);
+  if (!conn) {
+    SPE_LOG_ERR("spe_conn_create error");
+    spe_sock_close(cfd);
+    return;
+  }
+  spe_rds_t* rds = calloc(1, sizeof(spe_rds_t));
+  if (!rds) {
+    SPE_LOG_ERR("calloc error");
+    spe_sock_close(cfd);
+    return;
+  }
+  rds->conn = conn;
+  rds->status = RDS_INIT;
+  spe_conn_readuntil(conn, "\r\n\r\n", SPE_HANDLER1(drive_machine, rds));
+}
+
+void
 run(spe_server_t* srv, unsigned cfd) {
   spe_conn_t* conn = spe_conn_create(cfd);
   if (!conn) {
@@ -81,7 +165,7 @@ run(spe_server_t* srv, unsigned cfd) {
 static spe_server_conf_t srv_conf = {
   NULL,
   NULL,
-  run,
+  run1,
   0,
   0,
 };
@@ -124,5 +208,4 @@ __attribute__((constructor))
 static void
 __rdspool_init(void) {
   spe_register_module(&rdspool_module);
-
 }
